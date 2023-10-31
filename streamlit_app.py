9258e463dea43787099506d5bd35d02b8eab3911
NileshKAgrawal <149407785+NileshKAgrawal@users.noreply.github.com>
2023-10-31 23:00:04 +0530

    Update streamlit_app.py
    Added if else and error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,19 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice Api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-#Text Entry Box to receive and send the Input to Fruityvice as Part of the API Call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
+try:
+  #Text Entry Box to receive and send the Input to Fruityvice as Part of the API Call
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 #import snowflake.connector
